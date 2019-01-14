03579d9df1687bd330916494394c47d3c6458150
AkashaSnow <43389653+AkashaSnow@users.noreply.github.com>
2019-01-14 16:35:56 -0600

    Customer (#2)
    * Book Object
    
    * Meet
    
    * Book Object

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,33 @@
+import java.util.List;
+/**
+ * This will hold the names of the books in a list
+ */
 public class Customer {
+    private String name;
+    private List books;
 
+    public Customer(String name, List<Book> books) {
+        this.name = name;
+        this.books = books;
+    }
+    public Customer(){
+        this.name = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List getBooks() {
+        return books;
+    }
+
+    public void setBooks(List books) {
+        this.books = books;
+    }
 }
+
